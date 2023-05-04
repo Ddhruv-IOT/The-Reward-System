705b919223b6931ef48a51ab016d15b870345ea1
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-05-04 20:32:00 +0530

    added basic question template

--- utils.py
+++ utils.py
@@ -6,16 +6,41 @@ Created on Mon Jul 10 19:34:16 2023
 """
 
 import streamlit as st
+import requests
+
 
 def md_runner(data):
     st.markdown(data, unsafe_allow_html=True)
 
 
-def question_template(st, question, placeholder, info_text, icon, key ):
+def is_valid_link(url):
+    if not url:
+        st.info("Can't be blank", icon="🛑")
+        return False
+
+    try:
+        response = requests.head(url)
+        if response.status_code != 200:
+            st.info("Unreachable", icon="🛑")
+            return False
+    except requests.exceptions.RequestException:
+        st.info("Wrong Link", icon="⛔")
+        return False
+
+    return True
+
+
+def question_template(st, question, placeholder, info_text, icon, callback=None, key=0):
+
     text_input = st.text_input(
         question,
         placeholder=placeholder,
         key=key
     )
+
+    if callback and not callback(text_input):
+        return "Wrong Inputs"
+
+    st.text(f"Your Input: {text_input}")
     st.info(info_text, icon=icon)
     return text_input
