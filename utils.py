4c4e000fa6382bd3602f52971d1f4dba180589ac
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-05-05 20:09:00 +0530

    link keyword validation

--- utils.py
+++ utils.py
@@ -12,6 +12,12 @@ import requests
 def md_runner(data):
     st.markdown(data, unsafe_allow_html=True)
 
+def is_keyword_present(text, keyword):
+    if keyword not in text:
+        st.info(f"Invalid input for this field as keyword @ {keyword} is not found", icon="🛑")
+        return False
+    return True
+
 
 def is_valid_link(url):
     if not url:
@@ -30,7 +36,7 @@ def is_valid_link(url):
     return True
 
 
-def question_template(st, question, placeholder, info_text, icon, callback=None, key=0):
+def question_template(st, question, placeholder, info_text, icon, callback=None, callback2=None, keyword= None, key=0):
 
     text_input = st.text_input(
         question,
@@ -40,6 +46,10 @@ def question_template(st, question, placeholder, info_text, icon, callback=None,
 
     if callback and not callback(text_input):
         return "Wrong Inputs"
+    
+    if callback2 and not callback2(text_input, keyword):
+        st.write("Wrong Keyword")
+        return "Wrong Data"    
 
     st.text(f"Your Input: {text_input}")
     st.info(info_text, icon=icon)
