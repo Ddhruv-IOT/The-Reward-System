10779f6451eed262d306f3f97b10ee13e5382e73
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-05-05 20:08:00 +0530

    rough exp

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 10 23:28:55 2023
+# # -*- coding: utf-8 -*-
+# """
+# Created on Mon Jul 10 23:28:55 2023
 
-@author: Asus
-"""
+# @author: Asus
+# """
 
-with open("./simple_questions.txt", encoding="utf8" ) as questions:
-    a1 = questions.read().split("\n\n\n")
-    for t in a1:
-        print(t.split("\n"))
+# with open("./simple_questions.txt", encoding="utf8" ) as questions:
+#     a1 = questions.read().split("\n\n\n")
+#     for t in a1:
+#         print(t.split("\n"))
+
+x = [1, '2', 3]
+z = x.pop(2)
+print(z)
+
+
+x = "abc hi kw: abc"
+if "kw:" in x:
+    print("yes")
