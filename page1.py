0e9c6b773f10c57486d166245586ba37b1a6f3d8
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-05-05 20:06:00 +0530

    trying to add link validations, but failing...

--- page1.py
+++ page1.py
@@ -6,38 +6,34 @@ Created on Mon Jul 10 19:17:29 2023
 """
 
 import streamlit as st
-from utils import md_runner, question_template, is_valid_link
+from utils import md_runner, question_template, is_valid_link, is_keyword_present
 
     
 def page1():
     md_runner("<h3>So, Let's begin with Starter Challanges!</h3>")  
-    i = 0
+    question_template(st, "question", "question", "question", "⛔", callback=is_valid_link, callback2=is_keyword_present, keyword="git",key=0)
     with open("./simple_questions.txt", encoding="utf8" ) as questions:
         a1 = questions.read().split("\n\n\n")
-        for t in a1:
+        for ix, t in enumerate(a1):
+            print(ix)
             args = (t.split("\n"))
+            print(args)
+            
             callback = None
             if "is_valid_link" in args:
-                callback_index = args.index("is_valid_link")
-                callback = is_valid_link
-                args.pop(callback_index)
-            question_template(st, *args, callback=callback, key=i)
-            i += 1
+                args.pop(args.index("is_valid_link"))
+                callback = is_valid_link # the function name is passed as a callback
             
-    
-    
-   # x = question_template(st, "Enter GitHub Profile Link:", "A valid link", "5 marks", "⚠️",  1, is_valid_link)
-    # md_runner(x)
-   # x = question_template(st, "Enter GitHub Profile Link:", "A valid link", "5 0marks:", "⚠️",  2)
-   # md_runner(x)
-    # x = question_template(st, "Enter LinkedIN Profile Link:", "A valid link", 2)
-    # md_runner(x)
-    # x = question_template(st, "Enter StackOverflow Profile Link:", "A valid link", 3)
-    # md_runner(x)
-    # x = question_template(st, "Enter your gmail ID:", "A valid link", 4)
-    # md_runner(x)
-    
-    
-    
-    
-    
+            keyword = None
+            callback2 = None
+            if "kw:" in args:
+                print("yes")
+                keyword = args.pop(args.index("kw:"))
+                keyword = keyword.split(":")[1]
+            
+            print(args)
+            
+
+            question_template(st, *args, callback=callback, key=t)
+            
+            
