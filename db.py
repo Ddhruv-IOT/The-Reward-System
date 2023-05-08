8012f115816f29bf81a956de2bd032d6b9b6a97d
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-05-08 10:09:00 +0530

    connection to atlas db

---
+++ db.py
@@ -0,0 +1,20 @@
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from mongoCollection import student_data
+from dbSecrets import db_cred
+
+uri = f"mongodb+srv://{db_cred}.mongodb.net/?retryWrites=true&w=majority"
+
+# Connect to MongoDB
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+database = client["student_database"]
+collection = database["students"]
+
+# # Insert the student document into the collection
+# collection.insert_one(student_data)
+
+# # Find a student by name
+# result = collection.find_one({"name": "John Doe"})
+# print(result)
