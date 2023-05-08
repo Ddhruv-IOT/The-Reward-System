4e43169b828dfe943cca5fd4831f86524f569c86
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-05-08 10:05:00 +0530

    test collection example

---
+++ mongoCollection.py
@@ -0,0 +1,12 @@
+# Create a student document
+
+student_data = {
+    "name": "John Doe",
+    "password": "password123",
+    "linkedin": "https://www.linkedin.com/in/johndoe",
+    "li_points": 10,
+    "github": "https://github.com/johndoe",
+    "gi_points": 20,
+    "email": "johndoe@example.com",
+    "epoints": 30
+}
