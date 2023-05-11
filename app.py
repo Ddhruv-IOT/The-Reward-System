5ec2020fbb2b34056c888540a00cf704c7fe6125
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-05-12 00:33:00 +0530

    dependcies and db connection tune up

--- app.py
+++ app.py
@@ -9,10 +9,16 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from page1 import page1
 from utils import md_runner
+from admin_page import adm_page
 
 import pickle
 from pathlib import Path
 import streamlit_authenticator
+
+if 'key' not in st.session_state:
+    st.session_state['key'] = 'value'
+
+st.write(st.session_state['key'])
 
 
 names = ["Ddhruv", "Ankita"]
@@ -33,17 +39,21 @@ if authentication_status == None:
     st.warning("Please enter your credentials")
 if authentication_status == True:
 
-    with st.sidebar:
-        choose = option_menu("RAIoT Rewards", ["About", "Level1", "Level2", "Level3", "Coming Soon!"],
-                            icons=['house', 'camera fill', 'kanban',
-                                    'book', 'person lines fill'],
-                            menu_icon="app-indicator", default_index=0,
-                            )
+    if name == "Ddhruv":
+         adm_page()
 
-    if choose == "About":
-        st.write(f"Hi {name}")
-        md_runner("<h4>Want be on Developer Spotlight ? </h2>")
-        md_runner("<h4>Complete the Challanges and Earn points!</h2>")
+    else:
+        with st.sidebar:
+            choose = option_menu("RAIoT Rewards", ["About", "Level1", "Level2", "Level3", "Coming Soon!"],
+                                icons=['house', 'camera fill', 'kanban',
+                                        'book', 'person lines fill'],
+                                menu_icon="app-indicator", default_index=0,
+                                )
 
-    if choose == "Level1":
-        page1()
+        if choose == "About":
+            st.write(f"Hi {name}")
+            md_runner("<h4>Want be on Developer Spotlight ? </h2>")
+            md_runner("<h4>Complete the Challanges and Earn points!</h2>")
+
+        if choose == "Level1":
+            page1()
