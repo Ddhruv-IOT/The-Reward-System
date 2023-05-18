c173d2fb28a7f2530d27d97a4975d9b6f3e209b3
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-05-18 12:05:00 +0530

    added option menu

--- admin_page.py
+++ admin_page.py
@@ -1,4 +1,18 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+from add_user import add_user
+
 
 def adm_page():
-    st.write("Admin Page")
+    # st.write("Admin Page")
+    with st.sidebar:
+        choose = option_menu(
+            "RAIoT Rewards",
+            ["Statics", "Add User", "Coming Soon!"],
+            icons=["house", "camera fill", "kanban", "book", "person lines fill"],
+            menu_icon="app-indicator",
+            default_index=0,
+        )
+
+    if choose == "Add User":
+        add_user()
