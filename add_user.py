156133e8d082ec7e4958c4748f86b785b3571162
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-05-26 08:30:00 +0530

    added a loader

--- add_user.py
+++ add_user.py
@@ -2,10 +2,12 @@ import streamlit as st
 import uuid
 import streamlit_authenticator
 
-from db import adduser
+from db import adduser, read_student_name
+from utils import md_runner
 
 last_email = []
 _collection = st.session_state["db_coll"]
+st.session_state["disable"] = False
 
 
 def generate_user_password():
@@ -20,12 +22,14 @@ def generate_user_password():
 
 
 def add_user():
-    st.write("Add User here")
+    read_student_name(_collection)
 
     with st.form("my_form"):
+        md_runner("<h3> 👨‍💻 Add a new User 👩‍💻</h3>")
         col1, col2 = st.columns(2)
         with col1:
             name = st.text_input("Enter User name")
+            print(st.session_state["disable"])
         with col2:
             email = st.text_input("Enter User email")
         submitted = st.form_submit_button("Submit")
@@ -41,21 +45,20 @@ def add_user():
         elif email in last_email:
             st.error("Current and Previous email cannot be same", icon="❌")
             return
+        with st.spinner('Adding new user...'):
+            
+            random_password = generate_user_password()
+            last_email.append(email)
+            hashes = streamlit_authenticator.Hasher([random_password]).generate()
+            status = adduser(_collection, name, email, hashes[0])
 
-        random_password = generate_user_password()
-        last_email.append(email)
-        # st.write(name, email, random_password)
-        hashes = streamlit_authenticator.Hasher([random_password]).generate()
-        status = adduser(_collection, name, email, hashes[0])
-
-        if not status:
-            st.warning("Failed to add new user. Try again.", icon="⚠️")
-            return
-        elif status == "emailError":
-            st.error("Email already exists", icon="❌")
-            return
-        st.success(f"New user added successfully, user id: {status.inserted_id}", icon="✅")
-        st.write(name, email, random_password)
-    
-    
-        
+            if not status:
+                st.warning("Failed to add new user. Try again.", icon="⚠️")
+                return
+            elif status == "emailError":
+                st.error("Email already exists", icon="❌")
+                return
+            st.success(
+                f"New user added successfully, user id: {status.inserted_id}", icon="✅"
+            )
+            st.write(name, email, random_password)
