e1c815bd701b46eaae0771358cda2a3957cd0fd2
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-05-28 09:30:00 +0530

    added HTML email body support

--- emailer.py
+++ emailer.py
@@ -17,30 +17,34 @@ def connect_SMPT():
 
 
 subject = "Congratulations"
-body = """
-<html>
-  <body>
-    <h2>Introducing the RAIoT Rewards Program!</h2>
-    <p>
-		Welcome to the Reward Program of RAIOT Labs! where your talents are recognized and celebrated.
-		We are thrilled to have you on board. Get ready for an exciting journey where you can showcase your skills and earn recognition.
-		Participate by completing tasks and answering questions to earn points. As you accumulate points, you will be awarded badges to acknowledge your achievements.
-		Your dedication and expertise will also be highlighted on our social media handles, giving you well-deserved recognition among our community.
-		Join us now and unlock a world of opportunities in the realm of innovation and technology!
-  	</p>
-    <p>Please find your ID and password below:</p>
-    <ul>
-      <li>Your ID: <strong>&lt;id&gt;</strong></li>
-      <li>Your Password: <strong>&lt;pwd&gt;</strong></li>
-    </ul>
-    <p>For more information, visit our website: <a href="https://www.ddhruv.com">www.ddhruv.com</a></p>
-  </body>
-</html>
-
-"""
 
 
-def send_email(s, email):
+def e_body(email, password):
+    body = f"""
+	<html>
+	<body>
+		<h2>Introducing the RAIoT Rewards Program!</h2>
+		<p>
+			Welcome to the Reward Program of RAIOT Labs! where your talents are recognized and celebrated.
+			We are thrilled to have you on board. Get ready for an exciting journey where you can showcase your skills and earn recognition.
+			Participate by completing tasks and answering questions to earn points. As you accumulate points, you will be awarded badges to acknowledge your achievements.
+			Your dedication and expertise will also be highlighted on our social media handles, giving you well-deserved recognition among our community.
+			Join us now and unlock a world of opportunities in the realm of innovation and technology!
+		</p>
+		<p>Please find your ID and password below:</p>
+		<ul>
+		<li>Your ID: <strong>{email}</strong></li>
+		<li>Your Password: <strong>{password}</strong></li>
+		</ul>
+		<p>For more information, visit our website: <a href="https://www.ddhruv.com">www.ddhruv.com</a></p>
+	</body>
+	</html>
+	"""
+    return body
+
+
+def send_email(s, email, password):
+    body = e_body(email, password)
     msg = MIMEMultipart()
     msg["From"] = email_id
     msg["To"] = email
@@ -59,5 +63,5 @@ def stop_SMPT(s):
 
 if __name__ == "__main__":
     s = connect_SMPT()
-    send_email(s, "ddhruvarora2+st@gmail.com")
+    send_email(s, "ddhruvarora2+st@gmail.com", "test_password")
     stop_SMPT(s)
